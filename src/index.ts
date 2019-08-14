b7041d872d28ec3aec216ff22eb896516821a940
liujing37 <liujing37@baidu.com>
2019-08-14 18:54:34 +0800

    gulp js2php

--- src/index.ts
+++ src/index.ts
@@ -55,46 +55,39 @@ export function js2php() {
 
 export function getUnescapePhp(param: any) {
   return through.obj(function(file:File, enc, cb) {
-    if (file.isStream()) {
-      this.emit('error', new PluginError(PLUGIN_NAME, 'Stream not supported!'));
-      return cb();
-    }
-  
-    if (file.isBuffer()) {
-      let path = param.path || './';
-      const unescape = `${path}/unescape.php`;
-      isFileExisted(unescape).catch(() => {
-        createFolder(unescape);
-        const unescapeContents = `<?php
-        function unescape($str) { 
-          $ret = ''; 
-          $len = strlen($str); 
-        
-          for ($i = 0; $i < $len; $i++) { 
-            if ($str[$i] == '%' && $str[$i+1] == 'u') {
-              $val = hexdec(substr($str, $i+2, 4)); 
-              if ($val < 0x7f) $ret .= chr($val); 
-              else if($val < 0x800) $ret .= chr(0xc0|($val>>6)).chr(0x80|($val&0x3f)); 
-              else $ret .= chr(0xe0|($val>>12)).chr(0x80|(($val>>6)&0x3f)).chr(0x80|($val&0x3f)); 
-              $i += 5; 
-            } 
-            else if ($str[$i] == '%') { 
-              $ret .= urldecode(substr($str, $i, 3)); 
-              $i += 2; 
-            }
-            else {
-              $ret .= $str[$i];
-            }
+    let path = param.path || './';
+    const unescape = `${path}/unescape.php`;
+    isFileExisted(unescape).catch(() => {
+      createFolder(unescape);
+      const unescapeContents = `<?php
+      function unescape($str) { 
+        $ret = ''; 
+        $len = strlen($str); 
+      
+        for ($i = 0; $i < $len; $i++) { 
+          if ($str[$i] == '%' && $str[$i+1] == 'u') {
+            $val = hexdec(substr($str, $i+2, 4)); 
+            if ($val < 0x7f) $ret .= chr($val); 
+            else if($val < 0x800) $ret .= chr(0xc0|($val>>6)).chr(0x80|($val&0x3f)); 
+            else $ret .= chr(0xe0|($val>>12)).chr(0x80|(($val>>6)&0x3f)).chr(0x80|($val&0x3f)); 
+            $i += 5; 
           } 
-          return $ret; 
+          else if ($str[$i] == '%') { 
+            $ret .= urldecode(substr($str, $i, 3)); 
+            $i += 2; 
+          }
+          else {
+            $ret .= $str[$i];
+          }
         } 
-        return unescape;
-        ?>`;
-        fs.writeFile(unescape, unescapeContents, (res: any) => {
-            res ? console.log('res', res) : null;
-        });
+        return $ret; 
+      } 
+      return unescape;
+      ?>`;
+      fs.writeFile(unescape, unescapeContents, (res: any) => {
+          res ? console.log('res', res) : null;
       });
-    }
+    });
     cb();
   })
 }
